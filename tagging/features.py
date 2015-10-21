4802e1cfd24770040307897e7006717f2c033ae2
Germán Ferrero <ferrero.gf@gmail.com>
2015-10-21 10:05:54 -0300

    ejercicio 6, features.

--- tagging/features.py
+++ tagging/features.py
@@ -16,3 +16,73 @@ def word_lower(h):
     """
     sent, i = h.sent, h.i
     return sent[i].lower()
+
+
+def word_istitle(h):
+    """Feature: word is title.
+
+    h -- a history.
+    """
+    sent, i = h.sent, h.i
+    return sent[i].istitle()
+
+
+def word_isupper(h):
+    """Feature: word is super.
+
+    h -- a history.
+    """
+    sent, i = h.sent, h.i
+    return sent[i].isupper()
+
+
+def word_isdigit(h):
+    """Feature: word is digit.
+
+    h -- a history.
+    """
+    sent, i = h.sent, h.i
+    return sent[i].isdigit()
+
+
+def prev_tags(h):
+    return h.prev_tags
+
+
+class NPrevTags(Feature):
+
+    def __init__(self, n):
+        """Feature: n previous tags tuple.
+
+        n -- number of previous tags to consider.
+        """
+        self.n = n
+
+    def _evaluate(self, h):
+        """n previous tags tuple.
+
+        h -- a history.
+        """
+        total_n = len(h.prev_tags)
+        return h.prev_tags[total_n-self.n:]
+
+
+class PrevWord(Feature):
+
+    def __init__(self, f):
+        """Feature: the feature f applied to the previous word.
+
+        f -- the feature.
+        """
+        self.f = f
+
+    def _evaluate(self, h):
+        """Apply the feature to the previous word in the history.
+
+        h -- the history.
+        """
+        if h.i == 0:
+            return 'BOS'
+        else:
+            h = History(h.sent, h.prev_tags, h.i-1)
+            return str(self.f(h))
