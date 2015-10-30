aaf049dad07ed9cf50c68c202a4e6b5ab24c0cd5
Germán Ferrero <ferrero.gf@gmail.com>
2015-10-30 16:14:42 -0300

    Ejercicio 1: calculo de unlabeled precision,recall,F1 en eval.py. Resultados para modelos baseline en README.

---
+++ parsing/README.md
@@ -0,0 +1,40 @@
+#Ejercicio 1
+
+Resultados de Precision, Recall y F1 para modelos baseline:
+
+- Flat
+    - Parsed 1444 sentences
+    - Labeled
+        - Precision: 99.93%
+        - Recall: 14.57%
+        - F1: 25.43%
+
+    - Unlabeled
+        - Precision: 100.00%
+        - Recall: 14.58%
+        - F1: 25.45%
+        Parsed 1444 sentences
+
+- RBranch
+    - Parsed Sents: 1444 sentences
+    - Labeled
+        - Precision: 8.81%
+        - Recall: 14.57%
+        - F1: 10.98%
+
+    - Unlabeled
+        - Precision: 8.87%
+        - Recall: 14.68%
+        - F1: 11.06%
+
+-LBranch
+    - Parsed Sents: 1444 sentences
+    - Labeled
+        - Precision: 8.81%
+        - Recall: 14.57%
+        - F1: 10.98%
+
+    - Unlabeled
+        - Precision: 14.71%
+        - Recall: 24.33%
+        - F1: 18.33%
--- parsing/scripts/eval.py
+++ parsing/scripts/eval.py
@@ -1,13 +1,16 @@
 """Evaulate a parser.
 
 Usage:
-  eval.py -i <file>
+  eval.py -i <file> [-m <sent_max_length>] [-n <sents_count>]
   eval.py -h | --help
 
 Options:
   -i <file>     Parsing model file.
   -h --help     Show this screen.
+  -m <sent_max_length> Eval only sents with less than m+1 words.
+  -n <sents_count> Eval only first n sents.
 """
+
 from docopt import docopt
 import pickle
 import sys
@@ -34,13 +37,21 @@ if __name__ == '__main__':
     model = pickle.load(f)
     f.close()
 
+    n = opts['-n']
+    m = opts['-m']
+
     print('Loading corpus...')
     files = '3LB-CAST/.*\.tbf\.xml'
     corpus = SimpleAncoraCorpusReader('ancora/ancora-2.0/', files)
     parsed_sents = list(corpus.parsed_sents())
 
+    if m:
+        parsed_sents = [sent for sent in parsed_sents if len(sent.leaves()) <= int(m)]
+    if n:
+        parsed_sents = parsed_sents[:int(n)]
+
     print('Parsing...')
-    hits, total_gold, total_model = 0, 0, 0
+    hits, u_hits, total_gold, total_model = 0, 0, 0, 0
     n = len(parsed_sents)
     format_str = '{:3.1f}% ({}/{}) (P={:2.2f}%, R={:2.2f}%, F1={:2.2f}%)'
     progress(format_str.format(0.0, 0, n, 0.0, 0.0, 0.0))
@@ -57,10 +68,20 @@ if __name__ == '__main__':
         total_gold += len(gold_spans)
         total_model += len(model_spans)
 
+        # compute unlabeled scores
+        u_gold_spans = set(list(map(lambda x: x[1:], list(gold_spans))))
+        u_model_spans = set(list(map(lambda x: x[1:], list(model_spans))))
+        u_hits += len(u_gold_spans & u_model_spans)
+
         # compute labeled partial results
         prec = float(hits) / total_model * 100
         rec = float(hits) / total_gold * 100
         f1 = 2 * prec * rec / (prec + rec)
+
+        # compute unlabeled partial results
+        u_prec = float(u_hits) / total_model * 100
+        u_rec = float(u_hits) / total_gold * 100
+        u_f1 = 2 * u_prec * u_rec / (u_prec + u_rec)
 
         progress(format_str.format(float(i+1) * 100 / n, i+1, n, prec, rec, f1))
 
@@ -70,3 +91,8 @@ if __name__ == '__main__':
     print('  Precision: {:2.2f}% '.format(prec))
     print('  Recall: {:2.2f}% '.format(rec))
     print('  F1: {:2.2f}% '.format(f1))
+    print('')
+    print('Unlabeled')
+    print('  Precision: {:2.2f}% '.format(u_prec))
+    print('  Recall: {:2.2f}% '.format(u_rec))
+    print('  F1: {:2.2f}% '.format(u_f1))
